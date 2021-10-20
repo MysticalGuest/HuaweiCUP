1c3207e786363f34353fd0bfa8f89aaa8c057d03
MysticalGuest <1317148109@qq.com>
2021-10-20 16:18:53 +0800

    Create test2.py

---
+++ Mission5/测试/test2.py
@@ -0,0 +1,44 @@
+# import sys
+# sys.path.append('../')
+from Mission5.extract import *
+
+
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 设置图例字号
+mpl.rcParams['legend.fontsize'] = 10
+
+# 方式2：设置三维图形模式
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# 测试数据
+# np.linspace用来创建等差数列
+# x = np.linspace(-4 * np.pi, 4 * np.pi, 30)
+# print(x)
+# y = x + np.random.randn(x.shape[-1]) * 0.7
+# z = x * x
+x = extract_locationX()
+print(x)
+y = extract_locationY()
+z = extract_locationZ()
+
+
+# 绘制图形
+ax.plot(x, y, z, label='parametric1 curve')
+# 数据拟合
+p_yx = np.polyfit(y, x, 5)
+x_out = np.polyval(p_yx, y)
+p_yz = np.polyfit(y, z, 5)
+
+z_out = np.polyval(p_yz, y)
+ax.plot(x_out, y, z_out, label='parametric2 curve')
+
+# 显示图例
+ax.legend()
+
+# 显示图形
+plt.show()
+
