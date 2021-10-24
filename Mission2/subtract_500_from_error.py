0dd20576d2de94552ac278ccc15f9de9e1b6fa84
MysticalGuest <1317148109@qq.com>
2021-10-24 15:05:08 +0800

    Create subtract_500_from_error.py

---
+++ Mission2/subtract_500_from_error.py
@@ -0,0 +1,74 @@
+"""
+@version: v1.0
+@author: 你说得很队 21102860168
+@license: Apache Licence
+@contact: mysticalguest@163.com
+@site: 
+@software: PyCharm
+@file: subtract_500_from_error.py
+@time: 2021/10/21 19:26
+"""
+import re
+
+import openpyxl
+from openpyxl import Workbook
+
+
+def judge(jl, ac):
+    for jd, a in zip(jl, ac):
+        if jd - a < 100:
+            jd -= 500
+
+
+def caculate(s, list_rows, location, av):
+    # 除标题之外的所有行
+    next_rows = list_rows[1:]
+
+    for row in next_rows:
+        be_A0 = int(row[1].value)
+        be_A1 = int(row[2].value)
+        be_A2 = int(row[3].value)
+        be_A3 = int(row[4].value)
+        be = [be_A0, be_A1, be_A2, be_A3]
+        a = [av[1], av[2], av[3], av[4]]
+
+        for i in range(4):
+            print(be[i], a[i].value)
+            if be[i] - a[i].value > 400:
+                be[i] -= 500
+        s.append([location[0], row[1].value, row[2].value, row[3].value, row[4].value,
+                  be[0], be[1], be[2], be[3]])
+
+
+def subtract(root_dir, file_name, save_path):
+    wb_a = openpyxl.load_workbook(file_name, data_only=True)
+    ws_a = wb_a.active
+    rows_a = ws_a.rows
+    # 全部数据
+    li_a = list(rows_a)
+    i = 1
+    with open("Tag坐标信息.txt", 'r', encoding='utf-8') as txtData:  # 打开文件夹中的文件内容
+        lines = txtData.readlines()
+
+        for l in lines[2:]:
+            print(l, end='')
+            if l != '\n':
+                res_wb = Workbook()
+                res_ws = res_wb.create_sheet()
+                sheet = res_wb.active
+                title = ["Tag编号", "A0", "A1", "A2", "A3", "A0修改后", "A1修改后", "A2修改后", "A3修改后"]
+                sheet.append(title)
+
+                loc = re.findall(r"\d+", l)
+
+                wb = openpyxl.load_workbook(root_dir + str(loc[0]) + ".异常.xlsx", data_only=True)
+                ws = wb.active
+                rows = ws.rows
+                # 全部数据
+                li = list(rows)
+
+                caculate(sheet, li, loc, li_a[i])
+                # 处理文件名
+                res_wb.save(save_path + str(loc[0]) + ".异常.xlsx")
+                i += 1
+    print("数据计算完成！")
